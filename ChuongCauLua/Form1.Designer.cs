032eee51395fef2e436372880606620ae3dd886e
Huệ Nguyễn Tấn <20521359@ms.uit.edu.vn>
2022-02-28 22:44:17 +0700

    add file

---
+++ ChuongCauLua/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿
+namespace ChuongCauLua
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.TimeLua = new System.Windows.Forms.Timer(this.components);
+            this.walktime = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TimeLua
+            // 
+            this.TimeLua.Interval = 30;
+            this.TimeLua.Tick += new System.EventHandler(this.TimeLua_Tick);
+            // 
+            // walktime
+            // 
+            this.walktime.Tick += new System.EventHandler(this.walktime_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::ChuongCauLua.Properties.Resources.WS2k;
+            this.pictureBox1.Location = new System.Drawing.Point(145, 113);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(141, 96);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = global::ChuongCauLua.Properties.Resources.WS2k;
+            this.pictureBox2.Location = new System.Drawing.Point(380, 113);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(141, 96);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Image = global::ChuongCauLua.Properties.Resources.WS2k;
+            this.pictureBox3.Location = new System.Drawing.Point(609, 113);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(141, 96);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(6, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1052, 109);
+            this.panel1.TabIndex = 3;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox4.Image = global::ChuongCauLua.Properties.Resources.logoS_removebg_preview;
+            this.pictureBox4.Location = new System.Drawing.Point(347, 2);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(326, 104);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(16, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(253, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nhấn Chuột Trái : để bắn lửa";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(16, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(214, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nhấn phím D: sang Phải";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(209, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nhấn phím A: sang Trái";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.BackgroundImage = global::ChuongCauLua.Properties.Resources.e99df9aa53529b0cc243;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1064, 528);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1082, 575);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(1082, 575);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer TimeLua;
+        private System.Windows.Forms.Timer walktime;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+    }
+}
+
---
+++ ChuongCauLua/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChuongCauLua
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap lua, NhanVat;
+        private Graphics graphicform;
+        private int x, y, fcol, frow, sttlua, xnhanvat, ynhanvat, stt;
+        private Keys KEY;
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode==Keys.D)
+            {
+                KEY = Keys.D;
+                walktime.Start();
+
+            }
+            if (e.KeyCode == Keys.A)
+            {
+                KEY = Keys.A;
+                walktime.Start();
+
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.D)
+            {
+
+                walktime.Stop();
+
+            }
+            if (e.KeyCode == Keys.A)
+            {
+
+                walktime.Stop();
+
+            }
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void walktime_Tick(object sender, EventArgs e)
+        {
+           if(KEY==Keys.D)
+            {
+               if(stt==5)
+                {
+                    stt = 6;
+                }
+               else
+                {
+                    stt = 5;
+                }
+                xnhanvat += 20;
+
+            }
+           if (KEY == Keys.A)
+           {
+                if (stt == 2)
+                {
+                    stt = 0;
+                }
+                else
+                {
+                    stt = 2;
+                }
+                xnhanvat -= 20;
+           }
+            Refresh();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+           
+            x = xnhanvat + 80;
+            y = ynhanvat + 20;
+            if (KEY == Keys.D)
+            {
+                x = xnhanvat + 80;
+                
+            }
+            if (KEY == Keys.A)
+            {
+                x = xnhanvat - 60;
+                
+            }
+            TimeLua.Start();
+
+        }
+
+        private Bitmap spritewalk;
+        private Bitmap spritequacaulua;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            xnhanvat = 90;
+            ynhanvat = 175;
+            
+            sttlua = 0;
+            stt = 0;
+
+            spritewalk = new Bitmap(Properties.Resources.sonicvip, new Size(273, 252));
+            spritequacaulua = new Bitmap(Properties.Resources.Lua, new Size(560, 280));
+
+
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void TimeLua_Tick(object sender, EventArgs e)
+        {
+            
+            
+           
+            if(sttlua>32)
+            {
+                sttlua = -1;
+                TimeLua.Stop();
+            }
+            sttlua++;
+            fcol = sttlua % 8;
+            frow = sttlua / 8;
+            if (KEY == Keys.D)
+            {
+                x += 4;
+            }
+
+            if (KEY == Keys.A)
+            {
+                x -= 4;
+            }
+            Refresh();
+            
+           
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            graphicform = e.Graphics;
+            
+            
+
+            NhanVat = new Bitmap(50, 120);
+            Graphics charac = Graphics.FromImage(NhanVat);
+            charac.DrawImage(spritewalk, 0, 0, new Rectangle((stt % 5) * 50, (stt / 5) * 120, 50, 120), GraphicsUnit.Pixel);
+            graphicform.DrawImageUnscaled(NhanVat, xnhanvat, ynhanvat);
+            charac.Dispose();
+
+
+
+            
+            lua = new Bitmap(70, 70);
+            Graphics g = Graphics.FromImage(lua);
+            g.DrawImage(spritequacaulua, 0, 0, new Rectangle(fcol * 70, frow * 70, 70, 70), GraphicsUnit.Pixel);
+            graphicform.DrawImageUnscaled(lua, x, y);
+            g.Dispose();
+
+        }
+    }
+}
